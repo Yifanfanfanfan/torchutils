661e4307d0295d387ea818dc5682357da98755d8
yifangong <ygong@ece.utoronto.ca>
2020-07-17 00:55:00 -0400

    Romve useless import.

--- calflops.py
+++ calflops.py
@@ -7,8 +7,6 @@ import sys
 sys.path.append('../')
 # from resnet_1d import ResNet50_1d
 # from resnet_1d_lite import ResNet50_1d_shrink
-from thop import profile
-import yaml
 import wdsr_b
 from option2 import parser
 from wdsr_b import *
